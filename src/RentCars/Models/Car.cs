fabb56063364898cc486107748b8cead1a73c56f
João Lee <joaumlimaum@gmail.com>
2023-07-18 10:26:47 -0300

    Requisito 6 concluído

--- src/RentCars/Models/Car.cs
+++ src/RentCars/Models/Car.cs
@@ -2,6 +2,27 @@ using RentCars.Types;
 namespace RentCars.Models;
 
 //6 - Faça a classe `Car` herdar de `Vehicle`
-public class Car
+public class Car : Vehicle
 {
+  public int Seats { get; set; }
+  public int Doors { get; set; }
+  public TractionType Traction { get; set; }
+  public BrakeType FrontBrake { get; set; }
+  public BrakeType RearBrake { get; set; }
+
+  //   public Car(int seats, int doors, TractionType traction, BrakeType frontBrake, BrakeType rearBrake)
+  //   {
+  //     Seats = seats;
+  //     Doors = doors;
+  //     Traction = traction;
+  //     FrontBrake = frontBrake;
+  //     RearBrake = rearBrake;
+  //   }
 }
+
+
+// Seats que deve ser do tipo Int;
+// Doors que deve ser do tipo Int;
+// Traction que deve ser do tipo TractionType;
+// FrontBrake que deve ser do tipo BrakeType;
+// RearBrake que deve ser do tipo BrakeType;
--- src/RentCars/Types/Enums/TractionType.cs
+++ src/RentCars/Types/Enums/TractionType.cs
@@ -7,7 +7,3 @@ public enum TractionType
   RearWheelDrive,
   AllWheelDrive
 }
-
-// O enum deve conter o campo FrontWheelDrive com valor 0
-// O enum deve conter o campo RearWheelDrive com valor 1
-// O enum deve conter o campo AllWheelDrive com valor 2
