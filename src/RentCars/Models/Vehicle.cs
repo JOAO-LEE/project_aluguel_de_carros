93ba3da616dc3f795e0d8b93fe62ec02c77d8104
João Lee <joaumlimaum@gmail.com>
2023-07-18 10:05:03 -0300

    Requisito 5 concluído

--- src/RentCars/Models/Vehicle.cs
+++ src/RentCars/Models/Vehicle.cs
@@ -51,14 +51,3 @@ public class Vehicle
   }
 
 }
-
-
-// Brand que deve ser do tipo string, ter valor padrão igual à "", deve ter get e set;
-// Model que deve ser do tipo string, ter valor padrão igual à "", deve ter get e set;
-// Price que deve ser do tipo decimal, deve ter get e set;
-// Fuel que deve ser do tipo FuelType, deve ter get e set;
-// EngineCapacity que deve ser do tipo int, deve ter get e set;
-// MainColor que deve ser do tipo Color, deve ter get e set;
-// Year que deve ser do tipo int, deve ter get e set;
-// PricePerDay que deve ser do tipo double, deve ter get e set;
-// IsRented que deve ser do tipo bool, ter valor padrão igual à false, deve ter get e set;
--- src/RentCars/Types/Enums/TractionType.cs
+++ src/RentCars/Types/Enums/TractionType.cs
@@ -3,4 +3,11 @@ namespace RentCars.Types;
 //5 - Crie um enum `TractionType`
 public enum TractionType
 {
+  FrontWheelDrive,
+  RearWheelDrive,
+  AllWheelDrive
 }
+
+// O enum deve conter o campo FrontWheelDrive com valor 0
+// O enum deve conter o campo RearWheelDrive com valor 1
+// O enum deve conter o campo AllWheelDrive com valor 2
