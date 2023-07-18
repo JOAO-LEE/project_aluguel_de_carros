febe653e1b8e511cff93aacfd9a3c5e5b61b6109
João Lee <joaumlimaum@gmail.com>
2023-07-18 10:38:29 -0300

    Requisito 8 concluído

--- src/RentCars/Models/Truck.cs
+++ src/RentCars/Models/Truck.cs
@@ -2,11 +2,11 @@ using RentCars.Types;
 
 namespace RentCars.Models
 {
-    public class Truck : Vehicle
-    {
-        public int Seats { get; set; }
-        public int Doors { get; set; }
-        public double LoadCapacity;
-        public BrakeType BrakeType { get; set; }
-    }
+  public class Truck : Vehicle
+  {
+    public int Seats { get; set; }
+    public int Doors { get; set; }
+    public double LoadCapacity;
+    public BrakeType BrakeType { get; set; }
+  }
 }
